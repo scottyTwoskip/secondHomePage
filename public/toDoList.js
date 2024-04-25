514432392baff3446552ac7144857c5c59f45866
scottyTwoskip <scottyeansor@gmail.com>
2024-04-25 10:56:40 -0600

    changed inputs into form and adjusted css

--- public/toDoList.js
+++ public/toDoList.js
@@ -1,2 +1,10 @@
-//target where its going to be entered and where it will be displayed
-//take the input from the text area and display it on the page
+//target where the to do is going to be entered and where it will be displayed
+// track the date, important?, text area text. 
+//display these on page when pressing add
+
+//variables
+const dateInput = document.getElementById('date')
+const importantInput = document.getElementById('important-checkbox')
+const textArea = document.querySelector('textarea')
+
+
